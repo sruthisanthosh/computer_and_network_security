605352bfd8ef84ad553dd35522734e69617a5497
Sruthi Santhosh <35186002+sruthisanthosh@users.noreply.github.com>
2025-08-28 01:24:14 +1000

    Update guess_num.py

--- guess_num.py
+++ guess_num.py
@@ -3,26 +3,37 @@ import random
 l = random.sample(range(10), 4)
 n=int((''.join([str(x) for x in l])))
 num=str(n)
+print(num)
 
-chance = 1
+chance = 0
 
-while chance <= 5:
-    print(f"Chance {chance}")
+while chance < 5:
+    pos=0
+    print(f"Chance {chance+1}")
     guess=str(input("Enter guess:"))
 
     if guess==num:
-        print("you guessed it right!")
+        result = 1
         break
         
     else:
         for i in range(len(guess)):
+
             if guess[i] in num:
-                print(f"{guess[i]} is present in pos {num.index(guess[i])}")
+                print(f"{guess[i]} is present")
+                if num.index(guess[i])==guess.index(guess[i]):
+                    pos=pos+1
+
             else:
                 print(f"{guess[i]} is not present")
-        chance = chance + 1
+        print(f"Position of {pos} are correct")
 
-if chance==5:
-    print("5 chances are done. GAME OVER!!")
+        result = 0
+        chance = chance + 1
+     
+if chance==5 and result==0:
+    print(f"5 chances are done. GAME OVER!! \nThe secret number is {num}")
+elif result==1:
+    print(f"You're correct!! The number is {num}")
 
 
